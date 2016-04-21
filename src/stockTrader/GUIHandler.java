2bd011065d904a2da31f017fb11a3f893aa5b811
Amali24 <apt41790@gmail.com>
2016-04-21 17:15:28 -0400

    struggling with Scene Builder

--- src/stockTrader/GUIHandler.java
+++
@@ -1,7 +0,0 @@
-package stockTrader;
-
-public class GUIHandler {
-	void importFiles() {
-		System.out.println("importFiles() method called");
-	}
-}
---
+++ src/stockTrader/StockGUIHandler.java
@@ -0,0 +1,9 @@
+package stockTrader;
+
+import javafx.scene.layout.AnchorPane;
+
+public class StockGUIHandler extends AnchorPane{
+	public void test(){
+		System.out.println("test");
+	}
+}
