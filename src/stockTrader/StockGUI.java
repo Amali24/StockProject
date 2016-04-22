6a2d9868db8bdcff48628bee44bef1fb872a3f6a
Amali24 <apt41790@gmail.com>
2016-04-22 11:32:12 -0400

    More GUI improvements

--- src/stockTrader/StockGUI.java
+++ src/stockTrader/StockGUI.java
@@ -15,23 +15,38 @@ public class StockGUI extends Application{
 	public void start(Stage primaryStage){
 		String[] symbolArray = {"test 1", "test 2"};
 		int numStocks = symbolArray.length;
-		MenuItem[] stocksHeld;
-		stocksHeld = new MenuItem[numStocks];
+		MenuItem[] stocksEdit;
+		MenuItem[] stocksDelete;
+		stocksEdit = new MenuItem[numStocks];
+		stocksDelete = new MenuItem[numStocks];
 		Menu file = new Menu("File");
-			Menu managePortfolio = new Menu("Portfolio Management");
-				MenuItem addStock = new MenuItem("Add a Stock");
-					addStock.setOnAction(e -> placeHolder());
-				Menu editStock = new Menu("Edit a Stock");
-				for (int i = 0; i < symbolArray.length; i++){	
-					stocksHeld[i] = new MenuItem(symbolArray[i]);
-					stocksHeld[i].setOnAction(e -> placeHolder());
-				}
-					editStock.getItems().addAll(stocksHeld);
-				Menu deleteStock = new Menu("Delete a Stock");
+			
+		Menu managePortfolio = new Menu("Portfolio Management");
+		
+		MenuItem addStock = new MenuItem("Add a Stock");
+		addStock.setOnAction(e -> placeHolder("add"));
 				
-				managePortfolio.getItems().addAll(addStock, editStock, deleteStock);
+		Menu editStock = new Menu("Edit a Stock");
+		for (int i = 0; i < symbolArray.length; i++){	
+			String tempSymbol = symbolArray[i]; 
+			stocksEdit[i] = new MenuItem(symbolArray[i]);
+			stocksEdit[i].setOnAction(e -> placeHolder(tempSymbol));
+		}
+		editStock.getItems().addAll(stocksEdit);
+		
+		Menu deleteStock = new Menu("Delete a Stock");
+		for (int i = 0; i < symbolArray.length; i++){	
+			String tempSymbol = symbolArray[i]; 
+			stocksDelete[i] = new MenuItem(symbolArray[i]);
+			stocksDelete[i].setOnAction(e -> placeHolder(tempSymbol));
+		}
+		deleteStock.getItems().addAll(stocksDelete);
+				
+		managePortfolio.getItems().addAll(addStock, editStock, deleteStock);
 				
 		MenuItem exitApp = new MenuItem("Quit");
+		exitApp.setOnAction(e -> CloseConfirm.display("Exit?", "Are you sure you would like to exit the program?"));
+		
 		file.getItems().addAll(managePortfolio, exitApp);
 		
 		Menu edit = new Menu("Edit");
@@ -51,8 +66,12 @@ public class StockGUI extends Application{
 		
 	}
 	
-	public void placeHolder() {
-		System.out.println("Place Holder Invoked");
+	public static void exitApp() {
+		System.out.println("Exit Method Invoked");
+	}
+
+	public void placeHolder(String symbol) {
+		System.out.println("Place Holder Invoked: " + symbol);
 	}
 
 	public static void main(String[] args){
