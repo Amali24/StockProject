553753e3e1636ebbd35ec6aeb9d9bc4bcdf038ad
andrew <andrew@Andrew-HP-Laptop>
2016-04-23 01:33:29 -0400

    Base GUI almost done, comments added. Need to add add/delete methods and
    add recommendations

--- src/stockTrader/StockGUI.java
+++ src/stockTrader/StockGUI.java
@@ -3,6 +3,8 @@ package stockTrader;
 import java.text.DecimalFormat;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -16,11 +18,12 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class StockGUI extends Application{
 	
+	// Text Fields for Displaying Stock Info
 	TextField tbsymbol = new TextField();
 	TextField tbprice = new TextField();
 	TextField tbvolume = new TextField();
@@ -38,78 +41,106 @@ public class StockGUI extends Application{
 	TextField tbexchange = new TextField();	  
 	TextField tbmovingav200Day = new TextField();
 	
-	Text name = new Text("NO STOCK LOADED");
+
+	
+	// Text Object to display name of stock, defaults to "No Stock Loaded"
+	Text name = new Text("No Stock Loaded");
 
 	
 	@Override
 	public void start(Stage primaryStage){
-		Stage window = primaryStage;
-		window.setMinWidth(250);
+		Stage window = primaryStage; // "re-name" primaryStage for ease of use
+		window.setMinWidth(730); // Establish minimum width of stage for even spacing throughout
 		
-		String[] symbolArray = {"FB", "GOOGL", "MSFT", "AAPL"};
-		int numStocks = symbolArray.length;
+		String[] symbolArray = {"FB", "GOOGL", "MSFT", "AAPL"}; // Holds symbols of held stocks
 		
-		MenuItem[] stocksEdit;
+		int numStocks = symbolArray.length; // number of stocks
+		
+		// Menu and Button Array to Dynamically Create Menus and Buttons
 		MenuItem[] stocksDelete;
 		Button[] stocksView;
-		
-		stocksEdit = new MenuItem[numStocks];
 		stocksDelete = new MenuItem[numStocks];
 		stocksView = new Button[numStocks];
 		
+		// File Menu
 		Menu file = new Menu("File");
-
+		// Portfolio Management sub-menu
 		Menu managePortfolio = new Menu("Portfolio Management");
-		
+		// Add Stock menuItem for adding new stock to portfolio
 		MenuItem addStock = new MenuItem("Add a Stock");
-		addStock.setOnAction(e -> StockGUIActionHandlers.placeHolder("add"));
-				
-		Menu editStock = new Menu("Edit a Stock");
+		addStock.setOnAction(e -> addStock());
 		
-		for (int i = 0; i < numStocks; i++){	
-			String tempSymbol = symbolArray[i]; 
-			stocksEdit[i] = new MenuItem(symbolArray[i]);
-			stocksEdit[i].setOnAction(e -> StockGUIActionHandlers.placeHolder(tempSymbol));
-		}
-		
-		editStock.getItems().addAll(stocksEdit);
-		
+		// Delete Stock sub-menu (dynamically lists currently added stocks
 		Menu deleteStock = new Menu("Delete a Stock");
 		
+		// Create Dynamic Menu
 		for (int i = 0; i < numStocks; i++){	
 			String tempSymbol = symbolArray[i]; 
 			stocksDelete[i] = new MenuItem(symbolArray[i]);
 			stocksDelete[i].setOnAction(e -> StockGUIActionHandlers.placeHolder(tempSymbol));
 		}
-		
+		// Add dynamic list of stocks to delete stock menu
 		deleteStock.getItems().addAll(stocksDelete);
-				
-		managePortfolio.getItems().addAll(addStock, editStock, deleteStock);
-				
-		MenuItem exitApp = new MenuItem("Quit");
-		exitApp.setOnAction(e -> CloseConfirm.display("Exit?", "Are you sure you would like to exit the program?"));
 		
+		// Add all sub menus to portfolio menu
+		managePortfolio.getItems().addAll(addStock, deleteStock);
+		
+		// Add exit command
+		MenuItem exitApp = new MenuItem("Quit");
+		//Display confirmation box if exit clicked
+		exitApp.setOnAction(e -> CloseConfirm.display("Exit?", "Are you sure you would like to exit the program?"));
+		//Add all to File menu
 		file.getItems().addAll(managePortfolio, exitApp);
 		
+		// Create about menu with menu item "credits"
 		Menu about = new Menu("About");
 		MenuItem credits = new MenuItem("Credits");
 		about.getItems().add(credits);
+		// Display Credits
 		credits.setOnAction(e -> AlertBox.display("About Stock Advisor", "Stock Advisor was created by Alessandra Shipman & Andrew Thomas"));
 		
+		// Create VBox or stock buttons
 		VBox leftBar = new VBox(5);
 		
+		// Dyanmically create button for each stock in array
 		for (int i = 0; i < numStocks; i++){
 			String tempSymbol = symbolArray[i];
 			stocksView[i] = new Button(symbolArray[i]);
+			stocksView[i].setMaxWidth(66);
+			stocksView[i].setMinWidth(66);
 			stocksView[i].setOnAction(e -> displayStockInfo(tempSymbol));
 		}
 		
+		// Add dynamic buttons to leftBar
 		leftBar.getChildren().addAll(stocksView);
 		
+		// Create two border panes
 		BorderPane bPane = new BorderPane();
 		BorderPane aPane = new BorderPane();
+		
+		// MenuBar holds previously created menus
 		MenuBar toolbar = new MenuBar(file, about);
-				
+		
+		
+		// Make text boxes uneditable
+		tbsymbol.setEditable(false);
+		tbprice.setEditable(false);
+		tbvolume.setEditable(false);
+		tbpe.setEditable(false);
+		tbeps.setEditable(false);
+		tbweek52low.setEditable(false);
+		tbweek52high.setEditable(false);
+		tbdaylow.setEditable(false);
+		tbdayhigh.setEditable(false);
+		tbmovingav50day.setEditable(false);
+		tbmarketcap.setEditable(false);
+		tbshortRatio.setEditable(false);
+		tbpreviousClose.setEditable(false);
+		tbopen.setEditable(false);
+		tbexchange.setEditable(false);
+		tbmovingav200Day.setEditable(false);
+		
+		// Create a label for each data field
 		Label symbol = new Label("Symbol: ");
 		Label price = new Label("Price: ");
 		Label volume = new Label("Volume: ");
@@ -127,10 +158,12 @@ public class StockGUI extends Application{
 		Label exchange = new Label("Exchange: ");	  
 		Label movingav200Day = new Label("Moving 200 Day Average: ");
 		
+		// Create Grid Pane for labels and class level textFields
 		GridPane gPane = new GridPane();
 		gPane.setVgap(5);
 		gPane.setHgap(5);
 		
+		// Add all labels and text fields to grid pane
 		gPane.add(symbol, 0, 0);
 		gPane.add(exchange, 0, 1);
 		gPane.add(price, 0, 2);
@@ -167,36 +200,86 @@ public class StockGUI extends Application{
 		gPane.add(tbmovingav50day, 3, 6);
 		gPane.add(tbmovingav200Day, 3, 7);
 		
-		name.setTextAlignment(TextAlignment.CENTER);
+		// Set Font parameters for title
 		name.setFont(Font.font("Helvetica", FontWeight.BOLD, 20));
 		
+		//Position name
 		aPane.setTop(name);
+		BorderPane.setAlignment(name, Pos.CENTER);
+		// Position grid pane with labels and fields
 		aPane.setCenter(gPane);
 		
+		// Position toolbar and other panes
 		bPane.setTop(toolbar);
 		bPane.setCenter(aPane);
 		bPane.setLeft(leftBar);
 		
+		
+		// Set some margins
+		BorderPane.setMargin(leftBar, new Insets(25, 10, 10, 10));
+		
+		
+		// Display scene
 		Scene scene = new Scene(bPane);
 		window.setTitle("Stock Advisor");
 		window.setScene(scene);
 		window.show();
-				
+						
 	}
 	
+	private void addStock() {
+		// TODO finish this method
+		
+		Stage window = new Stage();
+		window.initModality(Modality.APPLICATION_MODAL);
+		window.setTitle("Add a Stock");
+		window.setMinWidth(250);
+		
+		Label symbol = new Label("Symbol: ");
+		TextField symbolTB = new TextField();
+		
+		String symbolIn = symbolTB.getText(); 
+		
+		Button okay = new Button("Add");
+		okay.setOnAction(e -> addToStockArray(symbolIn));
+		
+		Button cancel = new Button("Cancel");
+		cancel.setOnAction(e -> window.close());
+		
+		GridPane pane = new GridPane();
+		
+		pane.add(symbol, 0, 0);
+		pane.add(symbolTB, 1, 0);
+		pane.add(okay, 0, 1);
+		pane.add(cancel, 1, 1);
+		
+		Scene scene = new Scene(pane);
+		window.setScene(scene);
+		window.showAndWait();
+		
+	}
+
+	private void addToStockArray(String symbolIn) {
+		//TODO write this shit
+	}
+
 	public void displayStockInfo(String symbol) {
+		// Get stock info for given symbol
 		Stock tempStock = StockFetcher.getStock(symbol);
 		
+		// Display name at top of window
 		name.setText(tempStock.getName());
 		
+		// Decimal formats, one for money, one for other large numbers (volume)
 		DecimalFormat money = new DecimalFormat("$ ###,###,###.00");
 		DecimalFormat bigNumber = new DecimalFormat("###,###,###");
 		
+		// Set all text fields with applicable format
 		tbsymbol.setText(tempStock.getSymbol());
 		tbprice.setText("" + money.format(tempStock.getPrice()));
 		tbvolume.setText("" + bigNumber.format(tempStock.getVolume()));
 		tbpe.setText("" + tempStock.getPe());
-		tbeps.setText("" + tempStock.getEps());
+		tbeps.setText("" + money.format(tempStock.getEps()));
 		tbweek52low.setText("" + money.format(tempStock.getWeek52low()));
 		tbweek52high.setText("" + money.format(tempStock.getWeek52high()));
 		tbdaylow.setText("" + money.format(tempStock.getDaylow()));
@@ -208,6 +291,8 @@ public class StockGUI extends Application{
 		tbopen.setText("" + money.format(tempStock.getOpen()));
 		tbexchange.setText(tempStock.getExchange());
 		tbmovingav200Day.setText("" + money.format(tempStock.getMovingav200Day()));
+		
+
 	}
 	
 	public static void main(String[] args){
